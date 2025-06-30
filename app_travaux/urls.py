d845d0ce105aa96ce38cb560738b32542766933f
etali-m <etalijunior15@gmail.com>
2025-06-30 16:04:01 +0100

    update modele RPAO

--- app_travaux/urls.py
+++ app_travaux/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('<int:project_id>/aao', AAOView.as_view(), name="aao-travaux"),
+    path('<int:project_id>/rpao', RPAOView.as_view(), name="rpao-travaux")
 ]
--- app_travaux/views.py
+++ app_travaux/views.py
@@ -55,3 +55,52 @@ class AAOView(GenericAPIView):
             'message': "Avis d'Appel d'Offre a été mis à jour correctement"
         }, status=status.HTTP_201_CREATED)
     
+
+class RPAOView(GenericAPIView):
+    serializer_class = RPAOSerializer
+    queryset = RPAO.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        project_id = kwargs.get('project_id')
+        queryset = self.get_queryset().filter(appel_offre=project_id)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        project_id = kwargs.get('project_id')
+        data = request.data.copy()
+        data['appel_offre'] = project_id
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            'data': serializer.data,
+            'message': "Avis d'Appel d'Offre enregsitré avec succès"
+        }, status=status.HTTP_201_CREATED)
+    
+    #Mise à jour d'un RPAO
+    def put(self, request, *args, **kwargs):
+        project_id = kwargs.get('project_id')
+        data = request.data.copy()
+        data['appel_offre'] = project_id
+
+        try:
+            instance = RPAO.objects.get(appel_offre=project_id)
+        except RPAO.DoesNotExist:
+            return Response(
+                {'detail': "L'avis d'appel d'offre n'existe pas"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = self.get_serializer(instance, data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({ 
+            'data': serializer.data,
+            'message': "Avis d'Appel d'Offre a été mis à jour correctement"
+        }, status=status.HTTP_201_CREATED)
+    
