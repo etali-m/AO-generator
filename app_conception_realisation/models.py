b7f2ba3ee28704cc32d8680c8e497c58618a6f83
etali-m <etalijunior0@gmail.com>
2026-08-17 18:01:30 +0100

    update for eliminatory criteria

--- app_conception_realisation/models.py
+++ app_conception_realisation/models.py
@@ -111,6 +111,8 @@ class RPAO(models.Model):
 
     # ===== F. Évaluation =====
     ref_24 = RichTextField(default='')    # Critères d'évaluation (éliminatoires et essentiels)
+    criteres_eliminatoires = RichTextField(default='')  # Détail des critères éliminatoires (référence 24)
+    criteres_essentiels = RichTextField(default='')     # Détail des critères essentiels (référence 24)
     ref_25 = RichTextField(default='')    # Grille détaillée des critères et sous-critères (NA, NT, NF, NG)
 
     # ===== G. Attribution et éthique =====
